82d0cf0ec8f1bdb819561d9f7747cd4ac11ec029
hSpajic <hrvoje.spajic@fer.hr>
2020-08-07 19:50:33 +0200

    Adding kernel convolution

--- camera_test.py
+++ camera_test.py
@@ -1,4 +1,12 @@
-import cv2, time
+import cv2, time, numpy as np
+
+sharpen = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
+
+largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
+
 
 video = cv2.VideoCapture(0)
 
@@ -9,12 +17,13 @@ while True:
 
     check, frame = video.read()
 
-    print(check)
-    print(frame)
+    # print(check)
+    # print(frame)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Capturing", gray)
-    
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.filter2D(frame, -1, largeBlur)
+    cv2.imshow("Capturing", frame)
+
     
     key = cv2.waitKey(1)
     if key == ord('q'):
