1f989d354d062e2ea97852ae1f34f932eda1caf5
LSpajic <lovro.spajic@protonmail.com>
2020-08-13 15:56:58 +0200

    Blob finder

---
+++ blob_finder.py
@@ -0,0 +1,31 @@
+import cv2, time, numpy as np
+
+video = cv2.VideoCapture(0)
+
+a = 0
+largeBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+detector =cv2.SimpleBlobDetector()
+while True:
+    a = a + 1
+
+    check, frame = video.read()
+    frame = cv2.filter2D(frame, -1, largeBlur)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    g_bijela = np.array([0,0,0], dtype=np.uint8)
+    do_bijela = np.array([255,255,30], dtype=np.uint8)
+
+    # print(check)
+    # print(frame)
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing", frame)
+
+    mask = cv2.inRange(hsv, g_bijela, do_bijela)
+    #ovdje ide kod za trazenje blobova 
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+print (a)
+video.release()
