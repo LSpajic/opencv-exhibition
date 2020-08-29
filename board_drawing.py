a20f8280d73fe0df94fc99c632ce10b03a754af1
hSpajic <hrvoje.spajic@fer.hr>
2020-08-29 16:23:24 +0200

    Creating template for board_drawing

---
+++ board_drawing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def draw_chess_board(img, a, b, c):
+    pass
+
+def main(image_path, points_data_path):
+    #ucitaj sliku
+    #ucitaj tocke
+    #pozovi draw_chess_board s slikom i tockama kao argumentima
+    #pokazi sliku sa sahovskom plocom
+    pass
+
+if __name__ == "__main__":
+    main()
